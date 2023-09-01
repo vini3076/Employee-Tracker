2d20c2741c565a8401362128e5e831b3f9d86055
vnavani <vinita.navani@gmail.com>
2023-09-01 08:37:59 -0700

    schema code

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the employee_db database.`)
+);
